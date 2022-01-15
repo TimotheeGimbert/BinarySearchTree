2c2d44d120f256d4345eb04e7dc9eba0e9cf5268
TimotheeGimbert <timothee.gimbert@free.fr>
2022-01-15 01:21:58 +0100

    fetNbFloors function

--- Tree.js
+++ Tree.js
@@ -2,31 +2,41 @@ class Tree {
 
   constructor(rootValue, datas) {
     this.nodes = [];
-    this.rootNode = rootValue;
-    let rootNode = new Node(rootValue, null, null);
-    this.nodes.push(rootNode);
+    this.rootNode = new Node(rootValue, null, null);
+    this.nodes.push(this.rootNode);
     this.datas = datas;
     datas.forEach(element => {
-      this.insertNode(element, rootNode);
+      this.insertNode(element, this.rootNode);
     });
   } 
 
   insertNode(value, parentNode) {
     if (value < parentNode.value) {
       if (parentNode.leftChild !== null) this.insertNode(value, parentNode.leftChild);
-      else if (parentNode.leftChild === null) {
-        const newNode = new Node(value, null, null);
-        parentNode.leftChild = newNode;
-      }
+      else parentNode.leftChild =  new Node(value, null, null);
     }
     else if (value > parentNode.value) {
       if (parentNode.rightChild !== null) this.insertNode(value, parentNode.rightChild);
-      else if (parentNode.rightChild === null) {
-        const newNode = new Node(value, null, null);
-        parentNode.rightChild = newNode;
-      }
+      else parentNode.rightChild =  new Node(value, null, null);
     }
-    else console.log('error: node has a rong value, probably already in the tree');
+    else console.log('error: node has a wrong value, probably already in the tree');
+  }
+
+  getNbFloors() {
+    let floorsLeft = 0;
+    let node = this.rootNode;
+    while (node.leftChild) {
+      floorsLeft++;
+      node = node.leftChild;
+    }
+    let floorsRight = 0;
+    node = this.rootNode;
+    while (node.rightChild) {
+      floorsRight++;
+      node = node.rightChild;
+    }
+    console.log(floorsLeft, 'floors left');
+    console.log(floorsRight, 'floors right');
   }
 
 }
@@ -43,3 +53,4 @@ class Node {
 var tree = new Tree(6, [4,2,9,5,1,8,3]);
 console.log(tree);
 console.log(tree.nodes);
+console.log(tree.getNbFloors());
